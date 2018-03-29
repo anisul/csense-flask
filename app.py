b476401dd43ef8487d7e44707d9242b70201e1f4
anisul islam <anis.2803@gmail.com>
2018-03-30 02:08:19 +0300

    localhost specifics commented.

--- app.py
+++ app.py
@@ -132,4 +132,4 @@ def get_user():
 if __name__ == "__main__":
     app.run()
 
-app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
+#app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
